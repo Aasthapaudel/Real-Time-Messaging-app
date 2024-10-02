08da0ae18fd30e0b8fb2eb2cd08213e9eab0828f
Aastha <pdlpangali111@gmail.com>
2024-10-02 20:30:16 +0545

    tinker and userfactory

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('avatar')->default('avatar\user_1077114.png');
+            $table->string('avatar')->default('avatar/user_1077114.png');
             $table->string('name');
-            $table->string('username');
+            $table->string('username')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
